97a147f78dd26fb0e668b842e31bc4dfa20bb4bf
rachmk <71355251+rachmk@users.noreply.github.com>
2021-10-19 18:15:35 -0500

    10/19/2021 - Rachel, Ran, Abigail

--- 337Assignment1.py
+++ 337Assignment1.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import util
 
 stream = open('gg2013.json')
 data = json.load(stream)
@@ -11,12 +12,13 @@ nominees = {}
 winners = {}
 
 hostKeywords = ["host"]
-awardKeywords = ["best"]
+awardKeywords = ["Best"]
 presenterKeywords = ["present"]
 nomineeKeywords = ["nominate", "nominee"]
 winnerKeywords = ["won", "wins", "winner"]
 
-badKeywords = ["predict", "think", "thought", "bet", "guess", "wish", "knew", "know"]
+badKeywords = ["predict", "think", "thought", "bet", "guess", "wish", "knew", "know", "RT", "should"]
+filterKeywords = ["Globes", "Golden", "Golden Globes"]
 
 for tweet in data:
     goodTweet = True
@@ -28,16 +30,63 @@ for tweet in data:
             if word in tweet['text']:
                 string = tweet['text'].strip('.,;":!?)(][}{')
                 string = string.replace("'s", "")
-                for each in string.split():
+                string = string.split()
+                index = -1
+                for ind, c in enumerate(string):
+                    if word in c:
+                        index = ind
+                        break
+                words = util.new_search_backward(string, index)
+                for i in words:
+                    if i not in filterKeywords:
+                        if i in host and i[0].isupper():
+                            host[i] += 1
+                        elif i[0].isupper():
+                            host[i] = 1
+                words = util.new_search_forward(string, index)
+                for i in words:
+                    if i not in filterKeywords:
+                        if i in host and i[0].isupper():
+                            host[i] += 1
+                        elif i[0].isupper():
+                            host[i] = 1
+                """for each in string.split():
                     if each[0].isupper():
                         if each in host:
                             host[each] += 1
                         else:
-                            host[each] = 1
-        """ for word in awardKeywords:
+                            host[each] = 1"""
+        for word in awardKeywords:
             if word in tweet['text']:
-                awardNames.append(tweet['text'])
-        for word in presenterKeywords:
+                string = tweet['text'].strip('.,;":!?)(][}{')
+                string = string.replace("'s", "")
+                string = string.split()
+                index = -1
+                for ind, c in enumerate(string):
+                    if word in c:
+                        index = ind
+                        break
+                words = util.new_search_backward(string, index)
+                for i in words:
+                    if i not in filterKeywords:
+                        if i in awardNames and i[0].isupper():
+                            awardNames[i] += 1
+                        elif i[0].isupper():
+                            awardNames[i] = 1
+                words = util.new_search_forward(string, index)
+                for i in words:
+                    if i not in filterKeywords:
+                        if i in awardNames and i[0].isupper():
+                            awardNames[i] += 1
+                        elif i[0].isupper():
+                            awardNames[i] = 1
+                """for each in string.split():
+                    if each[0].isupper():
+                        if each in host:
+                            host[each] += 1
+                        else:
+                            host[each] = 1"""
+        """for word in presenterKeywords:
             if word in tweet['text']:
                 presenters.append(tweet['text'])
         for word in nomineeKeywords:
@@ -46,10 +95,16 @@ for tweet in data:
         for word in winnerKeywords:
             if word in tweet['text']:
                 winners.append(tweet['text']) """
-print(dict(sorted(host.items(), key=lambda item: item[1], reverse = True)))
-'''for i in range(20):
-    print(host[i])
-for i in range(20):
-    print(awardNames[i])
-for i in range(20):
-    print(presenters[i])'''
+
+keys = dict(sorted(host.items(), key=lambda item: item[1], reverse = True)).keys()
+values = dict(sorted(host.items(), key=lambda item: item[1], reverse = True))
+hosts = ""
+for ind, i in enumerate(keys):
+    if i in keys[ind + 1]:
+        hosts = values[keys[ind+1]]
+    else:
+        break
+print(hosts)
+'''print(i)
+if ind >= 20:
+    break'''
