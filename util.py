63ce298dfc8eee4de6b7cb26ce4777adee3f4016
rachmk <71355251+rachmk@users.noreply.github.com>
2021-10-19 18:15:52 -0500

    10/19/2021 - Rachel, Ran, Abigail

--- util.py
+++ util.py
@@ -27,3 +27,26 @@ s = 'I am really good at computer science'
 s_list = s.split()
 print(search_backward(s_list, 3, 12))
 
+def new_search_forward(data, starting_index):
+    if starting_index == len(data)-1:
+        return []
+    fragments = []
+    for index in range(starting_index + 1, len(data)):
+        fragments.append(" ".join(data[starting_index + 1:index + 1]))
+    return fragments
+
+s = 'I am really good at computer science'
+s_list = s.split()
+print(new_search_forward(s_list, 3))
+
+def new_search_backward(data, starting_index):
+    if starting_index == 0:
+        return []
+    fragments = []
+    for index in range(1, starting_index + 1):
+        fragments.append(" ".join(data[starting_index - index:starting_index]))
+    return fragments
+
+s = 'I am really good at computer science'
+s_list = s.split()
+print(new_search_backward(s_list, 3))
