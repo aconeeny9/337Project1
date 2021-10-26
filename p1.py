f68d52f00ec3ea33162e2663c4c6acb90a9e4ce7
rachmk <71355251+rachmk@users.noreply.github.com>
2021-10-26 18:01:32 -0500

    10/26/21 - Ran, Abigail, Rachel

--- p1.py
+++ p1.py
@@ -3,6 +3,7 @@ import json
 import util
 import imdb_checker as imdb
 import helpers
+import re
 
 stream = open('gg2013.json')
 data = json.load(stream)
@@ -21,14 +22,15 @@ all_nominated = {}
 hostKeywords = ["host"]
 awardKeywords = ["Best"]
 presenterKeywords = ["present"]
-nomineeKeywords = ["nominate", "nominee"]
+nomineeKeywords = ['nominate', 'nominee']
 #winnerKeywords = ["won", "wins", "winner"]
 winnerKeywords = ["won", "wins"]
 
-badKeywords = ["predict", "think", "thought", "bet", "guess", "wish", "knew", "know", "should"]
+badKeywords = ["predict", "think", "thought", "bet", "guess", "wish", "knew", "know", "should", "RT"]
 filterKeywords = ["Globes", "Golden", "Golden Globes", "#GoldenGlobes", "RT:", "goldenglobes"]
 punctuation = ['\'',',',';','"',':','!','?',')','(',']','[','}','{','#']
 replacements = [(" or ", "/"), ("Television","TV"),(" at "," ")]#, ("in a ", " ")]
+awardList = ['best motion picture - drama', 'best motion picture - musical or comedy', 'best performance by an actress in a motion picture - drama', 'best performance by an actor in a motion picture - drama', 'best performance by an actress in a motion picture - musical or comedy', 'best performance by an actor in a motion picture - musical or comedy', 'best performance by an actress in a supporting role in any motion picture', 'best performance by an actor in a supporting role in any motion picture', 'best director - motion picture', 'best screenplay - motion picture', 'best motion picture - animated', 'best motion picture - foreign language', 'best original score - motion picture', 'best original song - motion picture', 'best television series - drama', 'best television series - musical or comedy', 'best television limited series or motion picture made for television', 'best performance by an actress in a limited series or a motion picture made for television', 'best performance by an actor in a limited series or a motion picture made for television', 'best performance by an actress in a television series - drama', 'best performance by an actor in a television series - drama', 'best performance by an actress in a television series - musical or comedy', 'best performance by an actor in a television series - musical or comedy', 'best performance by an actress in a supporting role in a series, limited series or motion picture made for television', 'best performance by an actor in a supporting role in a series, limited series or motion picture made for television', 'cecil b. demille award']
 
 for tweet in data:
     goodTweet = True
@@ -107,12 +109,42 @@ for tweet in data:
                         helpers.add_to_nominees(all_nominated, frags, search_forward)
                     elif imdb.is_imdb_title(frags) and "Actor" not in string and "Actress" not in string:
                         helpers.add_to_nominees(all_nominated, frags, search_forward)
-        
+        for word in awardList:
+            if word in tweet['text'].lower():
+                clean_tweet = re.sub('\'s', '', tweet['text'])
+                clean_tweet = re.sub(r'[^\w\s]', '', clean_tweet)
+                split_string = clean_tweet.split()
+                
+                index = -1
+                for ind, c in enumerate(split_string):
+                    if word in c:
+                        index = ind
+                        break
+                # Search backward from keyword for actor/acress name
+                # search_backward = util.new_search_backward(split_string, index)
+                #  Search forward from keyword for award name
+                search_forward = util.new_search_forward(split_string, -1)
+                # if the fragment is an actor or actress, call helper to add frags to dict
+                for frags in search_forward:
+                    if "Present" in clean_tweet and imdb.is_imdb_person(frags):
+                        presenters[frags] = word
+                        #helpers.add_to_nominees(presenters, word, frags)
+                    elif "Actress" in clean_tweet and imdb.is_imdb_actress(frags):
+                        nominees[frags] = word
+                        #helpers.add_to_nominees(nominees, word, frags)
+                    elif "Actor" in clean_tweet and imdb.is_imdb_actor(frags):
+                        nominees[frags] = word
+                        #helpers.add_to_nominees(nominees, word, frags)
+                    elif imdb.is_imdb_title(frags) and "Picture" in clean_tweet or "Film" in clean_tweet:
+                        nominees[frags] = word
+                        #helpers.add_to_nominees(nominees, word, frags)
+
+# nominees = helpers.simplify_nominees(nominees)
+presenters = helpers.simplify_nominees(presenters)
 all_nominated = helpers.simplify_nominees(all_nominated)
 
-for item in all_nominated.items():
+for item in nominees.items():
     print(item)
-
 
 def compress_best_of_dict(aDictionary):
     keys = list(dict(sorted(aDictionary.items(), key=lambda item: item[1], reverse = True)).keys())
